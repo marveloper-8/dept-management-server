b241b1790cc4729fd6da46153d268c073f9f5ee9
Josh <marveloper.8@gmail.com>
2024-12-15 15:24:39 -0800

    env log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  console.log(`This is the env ${process.env.SUPABASE_URL}`)
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({
